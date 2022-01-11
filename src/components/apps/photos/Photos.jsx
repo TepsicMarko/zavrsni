95e9429ecf544cf698533d8c5a4f6b1ba21e61a3
Marko Tepsic <markotepshic@gmail.com>
2022-01-11 18:33:05 +0100

    added image pan and zoom in/out using panzoom library

--- src/components/apps/photos/Photos.jsx
+++ src/components/apps/photos/Photos.jsx
@@ -3,72 +3,83 @@ import Window from "../../system/window/Window";
 import WindowContent from "../../system/window/window-content/WindowContent";
 import { FileSystemContext } from "../../../contexts/FileSystemContext";
 import { ProcessesContext } from "../../../contexts/ProcessesContext";
-import { useState, useContext, useEffect, useRef, useMemo } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { HiOutlineZoomIn, HiOutlineZoomOut } from "react-icons/hi";
 import { BsTrash } from "react-icons/bs";
 import { GrRotateRight } from "react-icons/gr";
+import Panzoom from "@panzoom/panzoom";
 
 const Photos = ({ path }) => {
+  const imageContainerRef = useRef(null);
   const [src, setSrc] = useState("");
-  // const [zoom, setZoom] = useState(100);
-  // const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-  // const [aspectRatio, setAspectRatio] = useState(0);
-  const [rotation, setRotation] = useState(0);
+  const [zoom, setZoom] = useState(100);
+  const [panzoom, setPanzoom] = useState();
   const { readFileContent, deleteFSO } = useContext(FileSystemContext);
   const { endProcess } = useContext(ProcessesContext);
-  const imageRef = useRef(null);
 
   const setImageSource = (source) => {
     setSrc(source);
   };
 
-  // const zoomIn = () => {
-  //   setZoom(
-  //     zoom +
-  //       (zoom === 0
-  //         ? 1
-  //         : parseInt(
-  //             `${zoom}`.length < 3 ? `${zoom}`[0] : `${zoom}`.slice(0, -1)
-  //           ))
-  //   );
-  // };
+  const zoomIn = () => {
+    setZoom(
+      zoom + parseFloat(`${zoom}`.slice(0, `${parseInt(zoom)}`.length - 1)) <
+        6000
+        ? zoom + parseFloat(`${zoom}`.slice(0, `${parseInt(zoom)}`.length - 1))
+        : zoom
+    );
+  };
 
-  // const zoomOut = () => {
-  //   setZoom(
-  //     zoom -
-  //       (zoom === 0
-  //         ? 1
-  //         : parseInt(
-  //             `${zoom}`.length < 3 ? `${zoom}`[0] : `${zoom}`.slice(0, -1)
-  //           ))
-  //   );
-  // };
+  const zoomOut = () => {
+    setZoom(
+      zoom - parseFloat(`${zoom}`.slice(0, `${parseInt(zoom)}`.length - 1)) >=
+        100
+        ? zoom - parseFloat(`${zoom}`.slice(0, `${parseInt(zoom)}`.length - 1))
+        : 100
+    );
+  };
+
+  const resetZoom = () => setZoom(100);
 
   const deleteImage = () => {
     deleteFSO(path, "");
     endProcess("Photos");
   };
 
-  const rotate = () => {
-    setRotation(rotation + 90 !== 360 ? rotation + 90 : 0);
+  const handleMouseWhele = (e) => {
+    panzoom.zoomWithWheel(e);
+    setZoom(panzoom.getScale() * 100);
   };
+
+  useEffect(() => {
+    setPanzoom(
+      Panzoom(imageContainerRef.current, {
+        maxScale: 60,
+        minScale: 1,
+        cursor: "default",
+        panOnlyWhenZoomed: true,
+      })
+    );
+
+    const observer = new ResizeObserver(() => {
+      resetZoom();
+    });
+
+    observer.observe(imageContainerRef.current.parentElement);
+  }, []);
 
   useEffect(() => {
     path && readFileContent(path, setImageSource);
   }, [path]);
 
   useEffect(() => {
-    console.log("rotation changed");
-    // imageRef.current.style.transform = `rotate(${rotation}deg)`;
-  }, [rotation]);
-
-  // useEffect(() => {
-  //   setAspectRatio(imageRef.current.width / imageRef.current.height);
-  //   setDimensions({
-  //     width: imageRef.current.width,
-  //     height: imageRef.current.height,
-  //   });
-  // }, [src]);
+    if (panzoom) {
+      panzoom.zoom(zoom / 100, { animate: true });
+      if (zoom === 100) {
+        panzoom.pan(0, 0, { force: true, animate: true });
+      }
+    }
+  }, [zoom]);
 
   return (
     <Window
@@ -79,32 +90,14 @@ const Photos = ({ path }) => {
     >
       <WindowContent backgroundColor='#222222' flex flexDirection='column'>
         <div className='flex-center photos-toolbar'>
-          <HiOutlineZoomIn
-          // onClick={zoomIn}
-          />
-          <HiOutlineZoomOut
-          // onClick={zoomOut}
-          />
+          <HiOutlineZoomIn onClick={zoomIn} />
+          <HiOutlineZoomOut onClick={zoomOut} />
           <BsTrash onClick={deleteImage} />
-          <GrRotateRight onClick={rotate} />
         </div>
-        <div className='image-container'>
-          <img
-            ref={imageRef}
-            src={src}
-            // style={{
-            //   width:
-            //     dimensions.width > 0 ? dimensions.width * (zoom / 100) : "",
-            //   height:
-            //     dimensions.height > 0 ? dimensions.height * (zoom / 100) : "",
-            //   maxWidth:
-            //     dimensions.width > 0 ? dimensions.width * (zoom / 100) * 6 : "",
-            //   maxHeight:
-            //     dimensions.height > 0
-            //       ? dimensions.height * (zoom / 100) * 6
-            //       : "",
-            // }}
-          />
+        <div className='image-container' onWheel={handleMouseWhele}>
+          <div ref={imageContainerRef} className='performance-wrapper'>
+            <img id='test' src={src} />
+          </div>
         </div>
       </WindowContent>
     </Window>
