fbcc4ee80ab34c71a8ff9c45720e92ad26d8a49e
Marko Tepsic <markotepshic@gmail.com>
2022-02-09 15:30:54 +0100

    improved desktop grid behaviour, still has some bugs

--- src/hooks/useDesktopGrid.js
+++ src/hooks/useDesktopGrid.js
@@ -71,44 +71,22 @@ const useDesktopGrid = ({ maxColumns, maxRows }) => {
     return { row, column };
   };
 
-  const isNameTaken = (temp, name, i) => {
-    if (temp.hasOwnProperty(i > 0 ? `${name} (${i})` : name))
-      return isNameTaken(temp, name, i === 0 ? i + 2 : i + 1);
-    return i > 0 ? `${name} (${i})` : name;
-  };
-
   const calcCellPosition = (newColumnValue, newRowValue) => {
     if (newColumnValue <= 1) {
       // ako izalzi iz lijevog ruba ekrana
       newColumnValue = 1;
-    } else {
-      // ako ne izalzi iz lijevog ruba ekrana
-      if (newRowValue < 1 || newRowValue > maxRows) {
-        // a izlazi iz gornjeg
-        newColumnValue = newColumnValue; // dodaj 1 ak je okupirano
-        // ako izlazi iz donjeg ruba ekarana
-        if (newRowValue > maxRows) {
-          newRowValue = maxRows;
-        }
-      }
-
-      // ak izlazi iz desnog ruba ekrana
+    } else if (newColumnValue >= maxColumns) {
+      // ako izalzi iz desnog ruba ekrana
+      newColumnValue = maxColumns;
     }
 
     if (newRowValue <= 1) {
       // ako izalzi iz gornjeg ruba ekrana
       newRowValue = 1;
-    } else {
-      // ako ne izlazi iz gornjeg ruba ekarana
-      if (newColumnValue < 1 || newColumnValue > maxColumns) {
-        // a izlazi iz lijevog ili desnog ruba ekrana
-        newRowValue = newRowValue; // dodaj 1 ak je okupirano
-        if (newColumnValue > maxColumns) {
-          newColumnValue = maxColumns;
-        }
-      }
-
-      // ako izalzi iz gornjeg ruba ekrana
+    } else if (newRowValue >= maxRows) {
+      // ako izlazi iz donjeg ruba ekrana
+      newRowValue = 1;
+      newColumnValue += 1;
     }
 
     return {
@@ -117,19 +95,31 @@ const useDesktopGrid = ({ maxColumns, maxRows }) => {
     };
   };
 
-  const addToGrid = async (
-    items,
-    newCellPosition,
-    checkIsCellOccupied,
-    checkIsNameTaken
-  ) => {
+  const findFirstFreeCell = (name, cellPosition, newGrid) =>
+    new Promise(async (resolve, reject) => {
+      const { isOccupied } = await checkIsOccupied(name, cellPosition, newGrid);
+      console.log(isOccupied);
+      if (isOccupied) {
+        resolve(
+          findFirstFreeCell(
+            name,
+            calcCellPosition(cellPosition.column, cellPosition.row + 1)
+          ),
+          newGrid
+        );
+      } else {
+        console.log(cellPosition);
+        resolve(cellPosition);
+      }
+    });
+
+  const addToGrid = async (items, newCellPosition) => {
     if (!Object.keys(grid).length || !grid[items[0]]) {
       setGrid({ ...grid, [items[0]]: newCellPosition });
     } else {
       let newGrid = { ...grid };
       let movedColumns = 0;
       let movedRows = 0;
-      // checkIsNameTaken && (name = isNameTaken(newGrid, name, 0));
       if (newCellPosition.column !== grid[items[0]].column) {
         movedColumns = newCellPosition.column - grid[items[0]].column;
       }
@@ -137,41 +127,49 @@ const useDesktopGrid = ({ maxColumns, maxRows }) => {
         movedRows = newCellPosition.row - grid[items[0]].row;
       }
 
-      // console.log(movedColumns, movedRows);
-
-      for (let name of items.slice(0).reverse()) {
-        // console.log(items);
+      for (let name of items) {
         const newColumnValue = newGrid[name].column + movedColumns;
         const newRowValue = newGrid[name].row + movedRows;
-        console.log(name, newColumnValue, newRowValue);
+        // console.log(name, newColumnValue, newRowValue);
         const cellPosition = calcCellPosition(newColumnValue, newRowValue);
-        const { isOccupied, occupiedCellName } = await checkIsOccupied(
+
+        if (items.length > 1) newGrid[name] = cellPosition;
+        if (items.length === 1) {
+          const { isOccupied, occupiedCellName } = await checkIsOccupied(
+            name,
+            cellPosition,
+            newGrid
+          );
+          if (isOccupied) {
+            newGrid[name] = cellPosition;
+            await makeSpace(cellPosition, occupiedCellName, newGrid);
+          } else {
+            newGrid[name] = cellPosition;
+          }
+        }
+        // console.log(cellPosition, { ...newGrid });
+      }
+
+      for (let name of items) {
+        const { isOccupied } = await checkIsOccupied(
           name,
-          cellPosition,
+          newGrid[name],
           newGrid
         );
-        // console.log(
-        //   isOccupied,
-        //   name,
-        //   grid[name].column,
-        //   newCellPosition.column,
-        //   newColumnValue
-        // );
         if (isOccupied) {
-          console.log(cellPosition, newGrid);
-          newGrid[name] = cellPosition;
-          // if (items.length === 1)
-          await makeSpace(cellPosition, occupiedCellName, newGrid);
-        } else {
-          newGrid[name] = cellPosition;
-          console.log(cellPosition, { ...newGrid });
+          if (items.length > 1) {
+            const freeCellPosition = await findFirstFreeCell(
+              name,
+              { ...newGrid[name], row: newGrid[name].row + 1 },
+              newGrid
+            );
+            newGrid[name] = freeCellPosition;
+            console.log(freeCellPosition);
+          }
         }
       }
 
       setGrid(newGrid);
-      // newGrid[name] = gridPositon;
-      // checkIsCellOccupied && checkIsOccupied(gridPositon, newGrid, name);
-      // setGrid(newGrid);
     }
   };
 
